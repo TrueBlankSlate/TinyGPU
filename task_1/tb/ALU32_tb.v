75bb224199dd26a004429b446e753f8ae21ae664
athralk <atharva.alulkar@outlook.com>
2026-07-07 10:50:11 +0530

    fixed testbench

--- task_1/tb/ALU32_tb.v
+++ task_1/tb/ALU32_tb.v
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns/1ps
 module alu_tb;
     reg [31:0] a, b;
@@ -19,10 +21,10 @@ module alu_tb;
         #10 opcode=4'b0111;
         #10 a=32'hA0; b=2; opcode=4'b1000;
         #10 opcode=4'b1001;
-        #10 a=-32'd4; b=2; opcode=4'b1010;
+        #10 a=32'd4; b=2; opcode=4'b1010;
         #10 opcode=4'b1011;
-        #10 a=32'h000A; opcode=4'b1100;
-        #10 b=32'h000A; opcode=4'b1101;
+        #10 a=32'd0006; opcode=4'b1100;
+        #10 b=32'd0006; opcode=4'b1101;
         #10 a=7; b=6; opcode=4'b1111;
         #10 a=123; opcode=4'b0000;
         #10 $finish;
