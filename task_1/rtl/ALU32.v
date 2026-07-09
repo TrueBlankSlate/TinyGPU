431d90b286e65559696cd2a7f10fd5fc401a0fa1
athralk <atharva.alulkar@outlook.com>
2026-07-09 17:26:27 +0530

    fixed signed multiply and divide

--- task_1/rtl/ALU32.v
+++ task_1/rtl/ALU32.v
@@ -16,12 +16,12 @@ module ALU(
           	4'b0111: x = ~(a|b); //nor
             4'b1000: x = a>>b[4:0]; //right shift
             4'b1001: x = a<<b[4:0]; //left shift
-          	4'b1010: x = $signed(a)>>>b[4:0]; //arithmetic right shift
-          	4'b1011: x = $signed(a)<<<b[4:0]; //arightmetic left shift
+          	4'b1010: x = a>>>b[4:0]; //arithmetic right shift
+          	4'b1011: x = a<<<b[4:0]; //arightmetic left shift
             4'b1100: x = ~a; //not of a
             4'b1101: x = ~b; //not of b
-            4'b1111: x = a*b; //multiply
-            4'b0000: x = (b!=0)?a/b:32'b0; //divide a by b
+            4'b1111: x = $signed(a)*$signed(b); //multiply
+            4'b0000: x = (b!=0)?($signed(a)/$signed(b)):32'b0; //divide a by b
             default: x = 0;
         endcase
     end
