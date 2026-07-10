f01f0e51d69936dffad275f6d0f07a72e8df473a
athralk <atharva.alulkar@outlook.com>
2026-07-10 12:03:32 +0530

    adding register file and RTL folder

---
+++ task_3/rtl/register_file.v
@@ -0,0 +1,28 @@
+module RegisterFile(
+    input wire clk,
+    input wire we,
+
+    input wire [4:0] rs1_addr,
+    input wire [4:0] rs2_addr,
+    input wire [4:0] write_addr,
+
+    input wire [31:0] data,
+
+    output wire [31:0] read_data1,
+    output wire [31:0] read_data2
+);
+
+    // 32 registers, each 32 bits
+    reg [31:0] registers [0:31];
+
+    // Asynchronous Read
+    assign read_data1 = registers[rs1_addr];
+    assign read_data2 = registers[rs2_addr];
+
+    // Synchronous Write
+    always @(posedge clk) begin
+        if (we)
+            registers[write_addr] <= data;
+    end
+
+endmodule
