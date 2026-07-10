e7e4fe40ca06e7d2eeed525577084538f5121d6f
athralk <atharva.alulkar@outlook.com>
2026-07-10 18:43:07 +0530

    added reset logic

--- task_3/rtl/register_file.v
+++ task_3/rtl/register_file.v
@@ -1,6 +1,7 @@
 module RegisterFile(
     input wire clk,
     input wire we,
+    input rst,
 
     input wire [4:0] rs1_addr,
     input wire [4:0] rs2_addr,
@@ -18,9 +19,17 @@ module RegisterFile(
     assign read_data1 = registers[rs1_addr];
     assign read_data2 = registers[rs2_addr];
     
+    integer i;
     always @(posedge clk) begin
-        if (we)
-            registers[write_addr] <= data;
-    end
 
+        if (rst) begin
+            for(i = 0; i < 32; i=i+1) begin
+                registers[i] = 32'b0;
+            end
+        end
+
+        else if (we) begin
+            registers[write_addr] <= data;
+        end
+    end
 endmodule
