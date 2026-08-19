b1f26b610aa56913742204383f351a3d12483305
Aman Yadav <amanyadav20nov@gmail.com>
2026-08-19 20:15:51 +0530

    Update bridge.v

--- FPGA/UART/bridge.v
+++ FPGA/UART/bridge.v
@@ -1,7 +1,7 @@
 module bridge( 
     input clk,
-    input reg a00,a01,a11,a10,b00,b01,b11,b10
-    output reg mat_a[255:0],mat_b[255:0]  );
+    input reg [63:0]a00,a01,a11,a10,b00,b01,b11,b10,
+    output reg [255:0]mat_a,mat_b );
 
     always@( posedge clk) begin
         mat_a<={a00,a01,a11,a10};
